3a91e61b43f711f308a5629875e2f3c459d447a4
varun8487 <varunsonavne@gmail.com>
2021-03-16 16:53:27 +0530

    version 1

---
+++ coin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            FindObjectOfType<effect>().co_effect();
+            Destroy(gameObject);
+            
+        }
+
+    }
+}
